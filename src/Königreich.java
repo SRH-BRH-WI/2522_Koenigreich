2cb1ba2bbf1f5fe919266d26f319670c88d726cd
Dr. Dillinger <peter.dillinger@srh.de>
2023-04-19 15:31:11 +0200

    Ein paar Eingabeprobleme gefixt und den Einwohner abstrakt gemacht.

--- src/Königreich.java
+++ src/Königreich.java
@@ -4,12 +4,25 @@ public class Königreich {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
 
+        Hauptschleife:
         while (true) {
-
             System.out.print("Name des Einwohners: ");
             String name = eingabe.nextLine();
-            System.out.print("Einkommen des Einwohners: ");
-            int einkommen = eingabe.nextInt();
+            if ( name.isEmpty() ) break;
+            if ( nameEnthältSonderzeichen(name) ) {
+                System.out.println("Namen dürfen keine Sonderzeichen haben");
+                continue;
+            }
+
+            int einkommen = 0;
+            while (true) {
+                System.out.print("Einkommen des Einwohners: ");
+                if ( eingabe.hasNextInt() ) {
+                    einkommen = eingabe.nextInt();
+                    break;
+                }
+                eingabe.nextLine();
+            }
 
             System.out.print("Bevölkerungsgruppe (K, A, B, L): ");
             char gruppe = eingabe.next().toUpperCase().charAt(0);
@@ -26,13 +39,23 @@ public class Königreich {
                 case 'L': einwohner = new Leibeigen(name, einkommen);
                     break;
                 default:
-                    System.out.println("Ungültige Eingabe");
-                    einwohner = new Einwohner(name, einkommen); // ??
+                    System.out.println("Ungültige Eingabe, Programm wird beendet");
+                    break Hauptschleife;
             }
 
             System.out.println("Der Einwohner " + name + " zahlt " +
                     einwohner.steuer() + " Taler Steuern.");
             System.out.println();
         }
+        System.out.println("Vielen Dank und auf Wiedersehen");
+    }
+
+    private static boolean nameEnthältSonderzeichen(String name) {
+        for (int i=0; i < name.length(); i++) {
+            char aktuellesZeichen = name.charAt(i);
+            if ("'´#+*~.:,;^°<>|!/\\()[]{}&$§€@".indexOf(aktuellesZeichen) != -1)
+                return true;
+        }
+        return false;
     }
 }
